dc0c623e5927adb411f3a4b4d9b8788054faed59
jefflongo <jeff.a.longo@gmail.com>
2025-03-10 12:54:45 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# Paste and Indent
+
+Paste and Indent enables intelligent pasting of copied text such that it is automatically aligned to the current indentation level. There are several other extensions which attempt to serve a similar purpose; I found that none of them worked quite right, so I made my own.
+
+## Installation
+
+There is no special setup to use this extension. This extension simply shadows the default copy/paste keybinds.
+
+## How It Works
+
+When copying a block of text, the block is unindented before saving it to the clipboard. It does this by determining the minimum indentation level in the block, ignoring whitespace-only lines. Entire lines are considered when determining indentation level, even if the indentation is not included in the selection.
+
+When pasting, the indentation level is determined for the line that the cursor is on or where a selection is started. The clipboard text is reindented to this indentation level before pasting. Multiple cursors/selections are supported.
+
+## Limitations
+
+### Paste Indentation Is Not Determined by Language Features
+
+Paste indentation level is determined by examining existing indentation on the line where the cursor is present or where a selection is started. This means that the line must already be indented to the desired indentation level before pasting. This extension _does not_ look at surrounding context, such as `{` or `}` characters from preceding lines to determine indentation level.
+
+### Tabs Are Replaced With Spaces
+
+Currently, all indentation is replaced with spaces, regardless of whether the source content contained tabs or spaces.
+
+### Other Extensions That Shadow Copy/Paste
+
+This extension is not compatible with other extensions that shadow the copy/paste keybinds. One such extension is [Clipboard History](https://github.com/aefernandes/vscode-clipboard-history-extension). This is because there is no API for intercepting copy/paste outside of binding over the keybinds. To my knowledge, there is no way for extensions to share the keybinds (assuming the keybinds are in the same "when" context), nor would we want other extensions interacting with the clipboard.
+
+### Externally Copied Text
+
+If a block of text is copied from outside of VS Code, an attempt will be made to unindent the block before pasting. However, if the first line is not at the lowest level of indentation in the block and its indentation is not included in the selection, the block will not paste correctly. This is because the context from the first line is lost. For example, consider the following block of text:
+
+```python
+if foo:
+    bar()   # <-- selection starts just before bar, whitespace was NOT selected
+baz()       # <-- selection ends just after baz
+```
+
+If the copied selection consists of the calls to `bar` and `baz`, without including the indentation preceding `bar`, it is impossible to differentiate between the following pastes:
+
+```python
+# correct
+    bar()
+baz()
+```
+
+```python
+# incorrect
+bar()
+baz()
+```
+
+To reiterate, these _can_ be differentiated when this selection is copied within VS Code, where this extension has the context of the entire document, but cannot be differentiated when the text is copied externally. To work around this, be sure to copy leading whitespace when copying from outside of VS Code.
---
+++ src/extension.ts
@@ -0,0 +1,141 @@
+import * as vscode from 'vscode';
+
+async function copy() {
+	const editor = vscode.window.activeTextEditor;
+	if (!editor) {
+		return;
+	}
+
+	const eol = editor.document.eol === vscode.EndOfLine.LF ? "\n" : "\r\n";
+
+	// selections are in the order in which they are created. reorder by line since this is the
+	// order in which we will add things to the clipboard.
+	// before we do this, consider any selections of length zero as a request to copy the entire
+	// line.
+	const selections = editor.selections.map(selection => {
+		if (selection.isEmpty) {
+			const line = editor.document.lineAt(selection.start.line);
+			return new vscode.Selection(line.range.start, line.range.end);
+		}
+		return selection;
+	}).sort((a, b) => a.start.line - b.start.line);
+
+	const selectedLines: string[] = [];
+	let minimumIndentation: number | undefined = undefined;
+
+	for (const selection of selections) {
+		for (let i = selection.start.line; i <= selection.end.line; i++) {
+			// for the given line of this selection, get the entire line (whether the entire
+			// line is selected or not)
+			const line = editor.document.lineAt(i).text;
+
+			// strip all indentation from whitespace-only lines
+			if (line.trim().length === 0) {
+				selectedLines.push("");
+				continue;
+			}
+
+			// determine the amount of whitespace of this line
+			const indentation = line.match(/^(\s*)/)?.[0].length || 0;
+
+			// update the minimum indentation
+			if (minimumIndentation === undefined || indentation < minimumIndentation) {
+				minimumIndentation = indentation;
+			}
+
+			// save the selected portion of the line
+			if (i === selection.start.line || i === selection.end.line) {
+				const start = i === selection.start.line ? selection.start.character : 0;
+				const end = i === selection.end.line ? selection.end.character : line.length;
+				selectedLines.push(line.slice(start, end))
+			} else {
+				selectedLines.push(line);
+			}
+		}
+	}
+
+	// in case only whitespace lines are copied
+	if (minimumIndentation === undefined) {
+		minimumIndentation = 0;
+	}
+
+	// unindent the lines
+	const unindentedLines = selectedLines.map(line =>
+		line.replace(new RegExp(`^\\s{0,${minimumIndentation}}`), "")
+	).join(eol);
+
+	// write to the clipboard
+	await vscode.env.clipboard.writeText(unindentedLines);
+}
+
+async function paste() {
+	const clipboardText = await vscode.env.clipboard.readText();
+	if (!clipboardText) {
+		return;
+	}
+
+	const editor = vscode.window.activeTextEditor;
+	if (!editor) {
+		return;
+	}
+
+	const eol = editor.document.eol === vscode.EndOfLine.LF ? "\n" : "\r\n";
+
+	// split clipboard text into lines
+	const lines = clipboardText.split(/\n|\r\n|\n\r/);
+
+	// determine the minimum indentation in the clipboard.
+	// this step was performed in `copy`, but the text may have been copied from outside of vscode.
+	// make an attempt to unindent the copied text in this case, even though we don't have the
+	// context to properly handle the first line.
+	let minimumIndentation: number | undefined = undefined;
+	for (const line of lines) {
+		const indentation = line.match(/^(\s*)/)?.[0].length || 0;
+		if (minimumIndentation === undefined || indentation < minimumIndentation) {
+			minimumIndentation = indentation;
+		}
+	}
+
+	// in case only whitespace lines are copied
+	if (minimumIndentation === undefined) {
+		minimumIndentation = 0;
+	}
+
+	// unindent the lines
+	const preprocessedLines = lines.map(line =>
+		line.replace(new RegExp(`^\\s{0,${minimumIndentation}}`), "")
+	);
+
+	await editor.edit(editBuilder => {
+		// paste at each cursor/selection
+		editor.selections.forEach(selection => {
+
+			// get the indentation at the cursor
+			const firstLine = editor.document.lineAt(selection.start.line).text;
+			const textBeforeCursor = firstLine.slice(0, selection.start.character);
+			const indentation = textBeforeCursor.match(/^(\s*)/)?.[0] || ""
+
+			// apply indentation to lines that:
+			// 1. aren't the first line (it's already indented)
+			// 2. are non-empty
+			const indentedText = preprocessedLines
+				.map((line, index) =>
+					index !== 0 && line.trim().length !== 0 ? indentation + line : line)
+				.join(eol);
+
+			// replace the selected text (or insert if selection is empty)
+			editBuilder.replace(selection, indentedText);
+		});
+	})
+}
+
+export function activate(context: vscode.ExtensionContext) {
+	const disposables = [
+		vscode.commands.registerCommand("paste-and-indent.copy", copy),
+		vscode.commands.registerCommand("paste-and-indent.paste", paste)
+	];
+
+	context.subscriptions.concat(disposables);
+}
+
+export function deactivate() { }
