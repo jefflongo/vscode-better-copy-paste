cc2cc849a8a5209333fec6720f10067cdb69e8b0
jefflongo <jeff.a.longo@gmail.com>
2025-03-15 17:11:36 -0700

    add more unit tests

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -2,12 +2,19 @@ import * as assert from 'assert';
 import * as vscode from 'vscode';
 
 async function runCopyPaste(
-    input: string, copySelections: vscode.Selection[], pasteSelections: vscode.Selection[]) {
+    input: string,
+    copySelections: vscode.Selection[],
+    pasteSelections: vscode.Selection[],
+    cut: boolean = false) {
     const document = await vscode.workspace.openTextDocument({ content: input });
     const editor = await vscode.window.showTextDocument(document);
 
     editor.selections = copySelections;
-    await vscode.commands.executeCommand('copy-paste-and-indent.copy');
+    if (cut) {
+        await vscode.commands.executeCommand('copy-paste-and-indent.cut');
+    } else {
+        await vscode.commands.executeCommand('copy-paste-and-indent.copy');
+    }
 
     editor.selections = pasteSelections;
     await vscode.commands.executeCommand('copy-paste-and-indent.paste');
@@ -16,50 +23,118 @@ async function runCopyPaste(
 }
 
 suite("Extension Test Suite", () => {
-    test("Unindent multiline function call", async () => {
+    test("Indent basic", async () => {
         const input = [
-            'def foo():"',
-            '    print("',
-            '        "hello"',
-            '    )',
-            '',
-            '',
+            'hello world',
+            '    ',
         ].join("\n");
 
         const output = await runCopyPaste(
-            input, [new vscode.Selection(1, 4, 3, 5)], [new vscode.Selection(5, 0, 5, 0)]
+            input, [new vscode.Selection(0, 0, 0, 11)], [new vscode.Selection(1, 0, 1, 4)]
         );
         const expectedOutput = [
-            'def foo():"',
-            '    print("',
-            '        "hello"',
-            '    )',
-            '',
-            'print("',
-            '    "hello"',
-            ')',
+            'hello world',
+            '    hello world',
         ].join("\n");
 
         assert.strictEqual(output, expectedOutput);
     });
 
-    test("Unindent with leading spaces", async () => {
+    test("Unindent basic", async () => {
         const input = [
-            '        abcd',
-            '    e',
-            '',
-            '',
+            '        hello world',
+            '    ',
         ].join("\n");
 
         const output = await runCopyPaste(
-            input, [new vscode.Selection(0, 9, 1, 5)], [new vscode.Selection(3, 0, 3, 0)]
+            input, [new vscode.Selection(0, 0, 0, 19)], [new vscode.Selection(1, 0, 1, 4)]
         );
         const expectedOutput = [
-            '        abcd',
-            '    e',
+            '        hello world',
+            '    hello world',
+        ].join("\n");
+
+        assert.strictEqual(output, expectedOutput);
+    });
+
+    test("Unindent multiline", async () => {
+        const input = [
+            '        hello',
+            '            world',
+            '    ',
+        ].join("\n");
+
+        const output = await runCopyPaste(
+            input, [new vscode.Selection(0, 0, 1, 17)], [new vscode.Selection(2, 0, 2, 4)]
+        );
+        const expectedOutput = [
+            '        hello',
+            '            world',
+            '    hello',
+            '        world',
+        ].join("\n");
+
+        assert.strictEqual(output, expectedOutput);
+    });
+
+    test("Unindent multiline leading spaces", async () => {
+        const input = [
+            '            hello',
+            '        world',
+            '    ',
+        ].join("\n");
+
+        const output = await runCopyPaste(
+            input, [new vscode.Selection(0, 0, 1, 13)], [new vscode.Selection(2, 0, 2, 4)]
+        );
+        const expectedOutput = [
+            '            hello',
+            '        world',
+            '        hello',
+            '    world',
+        ].join("\n");
+
+        assert.strictEqual(output, expectedOutput);
+    });
+
+    test("Single line empty selection", async () => {
+        const input = [
+            'hello world',
+            '    foo bar baz',
+        ].join("\n");
+
+        const output = await runCopyPaste(
+            input, [new vscode.Selection(0, 0, 0, 0)], [new vscode.Selection(2, 7, 2, 7)]
+        );
+        const expectedOutput = [
+            'hello world',
+            '    hello world',
+            '    foo bar baz',
+        ].join("\n");
+
+        assert.strictEqual(output, expectedOutput);
+    });
+
+    test("Spread", async () => {
+        const input = [
+            '    hello',
+            'world',
             '',
-            '    bcd',
-            'e'
+            '',
+            '    ',
+        ].join("\n");
+
+        const output = await runCopyPaste(
+            input,
+            [new vscode.Selection(0, 0, 0, 0), new vscode.Selection(1, 0, 1, 0)],
+            [new vscode.Selection(2, 0, 2, 0), new vscode.Selection(4, 4, 4, 4)],
+        );
+        const expectedOutput = [
+            '    hello',
+            'world',
+            'hello',
+            '',
+            '    world',
         ].join("\n");
 
         assert.strictEqual(output, expectedOutput);
